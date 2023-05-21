d8cd2b1d48c2ea77357e937be65e28e843158ced
Aditya Narayan nayak <72183256+Aditya-Narayan-Nayak@users.noreply.github.com>
2023-05-21 13:20:30 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # install docker
-sudo apt-get update
-sudo apt-get -y install ca-certificates curl gnupg lsb-release
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+apt-get update
+apt-get -y install ca-certificates curl gnupg lsb-release
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-sudo apt-get update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+chmod a+r /etc/apt/keyrings/docker.gpg
+apt-get update
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 #Manage Docker as a non-root user
-sudo groupadd docker || true
-sudo usermod -aG docker $USER
+groupadd docker || true
+usermod -aG docker $USER
 newgrp docker << END
